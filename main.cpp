2454d79cbf65b095dd23e6a595eafae8c8891ff3
Rjcloud0125 <78168097+Rjcloud0125@users.noreply.github.com>
2021-04-27 12:57:22 -0400

    Adding source file main
    empty main function  along with the person struct

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct person{  // define the UDT 
+    
+    string pName;
+    long pSSN; 
+
+    char gender; 
+    string DOB;
+    
+    float height; 
+    float weight;
+    
+    long fSSN;
+    long mSSN;
+    
+
+    person* next;
+};
+
+int main()
+{
+  
+ 
+  return 0;
+}
