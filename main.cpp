0c93b02cecaf93a12bf557063e26e1186f2818a6
ZachAttack20012019 <75655726+ZachAttack20012019@users.noreply.github.com>
2021-04-28 08:14:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,41 @@ struct person{  // define the UDT
 
 int main()
 {
-  
+   person* head = new person; // independent person 
+    head->next = NULL; 
+    person* current = head; 
+    person* newPerson;
+    
+    // assign data values 
+    string fLine;
+    ifstream fMovStar;
+    fMovStar.open("persondata.txt"); 
+    
+    while (!fMovStar.eof())
+    {
+        getline( fMovStar, fLine); 
+        current->pname = fLine;
+        getline( fMovStar, fLine); 
+        current->SSN = stol(fLine);
+        getline( fMovStar, fLine); 
+        current->gender = fLine[0];
+        getline( fMovStar, fLine); 
+        current->DOB = fLine;
+        getline( fMovStar, fLine); 
+        current->height = stof(fLine);
+        getline( fMovStar, fLine); 
+        current->weight = stof(fLine);
+        getline( fMovStar, fLine); 
+        current->fName = fLine;
+        getline( fMovStar, fLine); 
+        current->mName = fLine;
+        
+        newPerson = new person; // independent person 
+        current->next = newPerson; // linking persons 
+        current = newPerson; 
+    }
+    
+    fMovStar.close(); 
  
   return 0;
 }
